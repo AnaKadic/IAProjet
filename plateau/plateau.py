f3473c78de0eb2829e4a12db2217ada9f1b13928
Athe03 <162315826+Athe03@users.noreply.github.com>
2024-04-30 18:28:29 +0200

    Update plateau.py

--- plateau/plateau.py
+++ plateau/plateau.py
@@ -62,13 +62,13 @@ class Plateau:
 
         for idx, ligne in enumerate(self.plateau):
             # Ligne de séparation
-            print(Fore.BLUE + " +" + "---+" * self.taille + Style.RESET_ALL)
+            print(Fore.BLUE + "  +" + "---+" * self.taille + Style.RESET_ALL)
 
             # Affiche le numéro de la ligne à gauche du plateau et les barres verticales en vert
             row_display = Fore.BLUE + f"{idx+1:2d}|"  # Vert pour le numéro de la ligne
             row_display += " | ".join(
                 PIERRE_NOIRE if cell == 'B' else PIERRE_BLANCHE if cell == 'N' else VIDE for cell in ligne
-            ) + " |" + Style.RESET_ALL
+            ) + " | " + Style.RESET_ALL
 
             print(Fore.BLUE + row_display + Style.RESET_ALL)
 
